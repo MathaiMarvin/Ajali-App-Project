a32931f2f36110293316dc897d28fec81c9caa9f
ryan605 <langatryan26@gmail.com>
2023-04-18 12:22:50 +0300

    merge with development branch

--- API/app/models/video.rb
+++
@@ -1,4 +0,0 @@
-class Video < ApplicationRecord
-    belongs_to :incidents
-    has_one_attached :video
-end
--- api/app/controllers/users_controller.rb
+++ api/app/controllers/users_controller.rb
@@ -1,2 +1,44 @@
 class UsersController < ApplicationController
+    # protect_from_forgery with: :null_session
+    def index 
+        users = User.all
+        render json: users, status: :ok
+    end
+
+    def create
+        user = User.create(userParams)
+        if user.valid?
+            token = encode(user.id, user.email, user.role)
+            app_response(message:'Registration was successful', status:200, data:{user: user, token: token})
+        else
+            app_response(message:'failed', status:400, data:{info: 'Invalid credentials'})
+        end
+    end
+
+    def show
+        user = User.find_by(id: params[:id])
+        if user
+            render json: user, status: :ok
+        else
+            render json: {message: "user not found"},status: :not_found
+        end
+
+    end
+
+
+    def login
+        sql = "username = :username OR email = :email"
+        user = User.where(sql, {username: userParams[:username], email: userParams[:email]}).first
+        if user && user.authenticate(userParams[:password])
+            token = encode(user.id, user.email, user.role)
+            app_response(message:'Login was successful', status:200, data:{user: user, token: token})
+        else
+            app_response(message:'failed', status:400, data:{info: 'Invalid credentials'})
+        end
+    end
+
+    def userParams
+        params.require(:user).permit(:email, :username, :password, :phone_number)
+    end
+
 end
