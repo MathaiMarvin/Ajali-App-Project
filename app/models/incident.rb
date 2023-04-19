3da9fd21efb1c1becb15f413b6a4409622c2be2a
RichardOkongo <rirachard19@gmail.com>
2023-04-19 10:41:50 +0300

    file restructuring

--- app/models/incident.rb
+++
@@ -1,7 +0,0 @@
-class Incident < ApplicationRecord
-    belongs_to :user
-    has_many :images, dependent: :destroy
-    has_many :videos, dependent: :destroy
-    has_many :geolocations, dependent: :destroy
-    enum status: { under_investigation: 0, rejected: 1, resolved: 2 }
-end
