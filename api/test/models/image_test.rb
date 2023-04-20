30dbfe265086455639b2d0f7c823f39d33bdffc6
Knoxx04 <onyiknoxx3@gmail.com>
2023-04-20 05:17:53 +0300

    image_test.rb complete

--- api/test/models/image_test.rb
+++ api/test/models/image_test.rb
@@ -1,7 +1,20 @@
-require "test_helper"
+require 'test_helper'
 
 class ImageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should belong to an incident" do
+    image = Image.new
+    assert_not image.save, "Saved the image without an incident"
+    
+    incident = Incident.new
+    incident.images << image
+    assert image.incident == incident, "Image does not belong to the incident"
+  end
+  
+  test "should have an attached image" do
+    image = Image.new
+    assert_not image.save, "Saved the image without an attached image"
+    
+    image.image.attach(io: File.open(Rails.root.join('test', 'fixtures', 'files', 'test-image.jpg')), filename: 'test-image.jpg', content_type: 'image/jpg')
+    assert image.image.attached?, "Image does not have an attached image"
+  end
 end
