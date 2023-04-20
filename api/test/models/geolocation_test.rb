c004eb75d2a3832bb8b225daa0bf2c4a467203a1
Knoxx04 <onyiknoxx3@gmail.com>
2023-04-20 05:04:14 +0300

    geolocation_test.rb complete

--- api/test/models/geolocation_test.rb
+++ api/test/models/geolocation_test.rb
@@ -1,7 +1,26 @@
-require "test_helper"
+require 'test/unit'
+require_relative 'geolocation'
+require_relative 'incident'
 
-class GeolocationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+class GeolocationTest < Test::Unit::TestCase
+  def setup
+    @incident = Incident.create(title: "Test incident")
+    @geolocation = Geolocation.create(latitude: 37.7749, longitude: -122.4194, incident: @incident)
+  end
+
+  def teardown
+    Incident.destroy_all
+    Geolocation.destroy_all
+  end
+
+  def test_should_have_latitude_and_longitude
+    assert_equal 37.7749, @geolocation.latitude, "Latitude was not set correctly"
+    assert_equal(-122.4194, @geolocation.longitude, "Longitude was not set correctly")
+  end
+
+  def test_should_belong_to_an_incident
+    assert_instance_of Incident, @geolocation.incident, "Geolocation does not belong to an Incident"
+    assert_equal @incident, @geolocation.incident, "Geolocation belongs to the wrong Incident"
+  end
 end
+
