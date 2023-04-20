8ee781f683d5551a97353b2c790d510ef97ab39c
ryan605 <langatryan26@gmail.com>
2023-04-20 13:59:06 +0300

    tests

--- api/app/controllers/users_controller.rb
+++ api/app/controllers/users_controller.rb
@@ -1,44 +1,26 @@
 class UsersController < ApplicationController
-    # protect_from_forgery with: :null_session
-    def index 
-        users = User.all
-        render json: users, status: :ok
-    end
-
     def create
-        user = User.create(userParams)
-        if user.valid?
-            token = encode(user.id, user.email, user.role)
-            app_response(message:'Registration was successful', status:200, data:{user: user, token: token})
-        else
-            app_response(message:'failed', status:400, data:{info: 'Invalid credentials'})
-        end
-    end
-
-    def show
-        user = User.find_by(id: params[:id])
+        user = User.create!(userParams)
         if user
-            render json: user, status: :ok
+            save_user(user.id)
+            render json: user, status: :created
         else
-            render json: {message: "user not found"},status: :not_found
+            render json: {error: user.error}, status: :unprocessible_entity
         end
-
     end
-
 
     def login
-        sql = "username = :username OR email = :email"
-        user = User.where(sql, {username: userParams[:username], email: userParams[:email]}).first
-        if user && user.authenticate(userParams[:password])
-            token = encode(user.id, user.email, user.role)
-            app_response(message:'Login was successful', status:200, data:{user: user, token: token})
+        user = User.find_by(email: params[:email])
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: {message: "WELCOME"}, status: :ok
         else
-            app_response(message:'failed', status:400, data:{info: 'Invalid credentials'})
+            render json: { error: "UNAUTHORIZED" }, status: :not_found
         end
     end
 
     def userParams
-        params.require(:user).permit(:email, :username, :password, :phone_number)
+        params.permit(:username,:email,:password,:phone_number)
     end
 
 end
