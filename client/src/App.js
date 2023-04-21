0b907ee9b8ca13cdd6aea51751df5cc7cb338743
ryan605 <langatryan26@gmail.com>
2023-04-21 10:20:36 +0300

    change

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import {Routes, Route} from 'react-router-dom'
 import './App.css';
 import Home from './components/Home';
 import Signup from './components/Signup';
+import LandingPageAdmin from './components/LangingPageAdmin';
 import LandingPageClient from './components/LandingPageClient';
 
 
@@ -10,8 +11,9 @@ function App() {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Home/>} />
+        {/* <Route path='/' element={<Home/>} /> */}
         <Route path='/signup' element={<Signup/>} />
+        <Route path='/admin'element ={<LandingPageAdmin/>}/>
         <Route path='/landingpageclient' element={<LandingPageClient/>} />
       </Routes>
     </div>
