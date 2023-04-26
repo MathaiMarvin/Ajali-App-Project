c389c747fdde226cb8eced3e7568ac291ac71ad7
MathaiMarvin <mathaimarvin574@gmail.com>
2023-04-26 09:45:11 +0300

    authorization

--- api/config/application.rb
+++ api/config/application.rb
@@ -12,6 +12,7 @@ module Ajali
     config.load_defaults 7.0
      config.middleware.use ActionDispatch::Cookies
      config.middleware.use ActionDispatch::Session::CookieStore
+     
 
     # Configuration for the application, engines, and railties goes here.
     #
