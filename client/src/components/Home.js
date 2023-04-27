49deedd93223a80b8c52170eda33677b2958bfed
MathaiMarvin <mathaimarvin574@gmail.com>
2023-04-28 00:35:55 +0300

    adding a chatbot

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -42,9 +42,51 @@ const Home = () => {
     //       }
     //     });
     //   };  
-    const navigate = useNavigate();
+//     const navigate = useNavigate();
 
-const [formData, setFormData] = useState ({
+// const [formData, setFormData] = useState ({
+//   username: "",
+//   password: "",
+//   role: "",
+// });
+// const [isLoading, setIsLoading] = useState(false);
+
+// const handleLogin = (e) => {
+//   e.preventDefault();
+//   setIsLoading(true);
+
+//   fetch("https://ajalireports.onrender.com/users/login", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(formData),
+//   }).then((response) => {
+//     if (response.ok) {
+//       response.json().then((data) => {
+//         localStorage.setItem("session", data.session);
+//         localStorage.setItem("user", JSON.stringify(data.user));
+//         console.log("Welcome");
+//         console.log(data);
+//         if (user.role === "admin") {
+//           navigate("/admin");
+//         } else if (data.role === "normal_user") {
+//           navigate("/landingpageclient");
+//         } else {
+//           console.log("Unauthorized access");
+//         }
+//         setIsLoading(false);
+//       });
+//     } else {
+//       console.log("username or password incorrect");
+//     }
+//   });
+// };
+
+
+const navigate = useNavigate();
+
+const [formData, setFormData] = useState({
   username: "",
   password: "",
   role: "",
@@ -68,9 +110,9 @@ const handleLogin = (e) => {
         localStorage.setItem("user", JSON.stringify(data.user));
         console.log("Welcome");
         console.log(data);
-        if (data.role === "admin") {
+        if (data.user && data.user.role === "admin") {
           navigate("/admin");
-        } else if (data.role === "normal_user") {
+        } else if (data.user && data.user.role === "normal_user") {
           navigate("/landingpageclient");
         } else {
           console.log("Unauthorized access");
