12ee27bba6585db8135b749cb4913e2c6b62610e
MathaiMarvin <mathaimarvin574@gmail.com>
2023-05-01 18:45:07 +0300

    fixing destroy error

--- api/app/models/incident.rb
+++ api/app/models/incident.rb
@@ -1,8 +1,7 @@
 class Incident < ApplicationRecord
 
     belongs_to :user
-    has_one_attached :image, dependent: :destroy
-    has_one_attached :video, dependent: :destroy
+    
     
     enum status: { under_investigation: 0, rejected: 1, resolved: 2 }
 end
